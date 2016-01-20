ab681c24657f24231a596a0bd9a3e1ccca5d6f16
bitfidget <kriss.heimanis@au.pwc.com>
2016-01-21 08:58:22 +1100

    saving before overwriting

--- assets/scripts/hamburger.js
+++ assets/scripts/hamburger.js
@@ -5,13 +5,29 @@ $(document).ready(function() {
 		$('#hamburger-container').toggleClass('active');
 	});
 
+	$('.hamburger-close').on('click touch', function(event) {
+		event.preventDefault();
+		$('#hamburger-container').toggleClass('active');
+	});
+
 	$('.subnav-toggle').on('click touch', function(event) {
 		event.preventDefault();
 		$('nav.subnav').toggleClass('toggled');
 	});
 
-	var thisHeight = $(window).innerHeight() - 300;
+	var winHeight = $(window).innerHeight()
+	var thisHeight = winHeight - 300;
+
+	if ($('#central-content').height() < winHeight) {
+		$('.btn-top').css('display', 'none' );
+	}
 
 	$('#central-content').css('min-height', thisHeight );
+
+	$("a[href='#top']").click(function() { 
+		$("html, body").animate({ 
+			scrollTop: 0 
+		}, "slow"); return false; 
+	});
 
 });
